2feb3abaca9f46efbb4e0f1f13ca7d772cf8f11e
Shubham Kumar <140595898+kumar-commits@users.noreply.github.com>
2024-06-13 19:53:33 +0000

    added access_token

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = 8000;
 const summarizeText = require('./summarize.js');
 const path = require('path')
 
